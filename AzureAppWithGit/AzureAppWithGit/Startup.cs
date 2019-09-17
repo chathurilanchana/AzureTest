3243b33b171861c58f62aa2b18292afe7b909f62
Chathuri Gunawardhana <lanch.gunawardhana@gmail.com>
2019-09-17 23:36:46 +0200

    version 6

--- AzureAppWithGit/AzureAppWithGit/Startup.cs
+++ AzureAppWithGit/AzureAppWithGit/Startup.cs
@@ -27,7 +27,7 @@ namespace AzureAppWithGit
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! unleash commit5 to git");
+                await context.Response.WriteAsync("Hello World! unleash commit6 to git");
             });
         }
     }
